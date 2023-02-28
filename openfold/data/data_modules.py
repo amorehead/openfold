2d071df6141826570b3fb16a5240f26f2c5614e6
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-03-01 00:14:02 +0100

    env

--- openfold/data/data_modules.py
+++ openfold/data/data_modules.py
@@ -602,7 +602,7 @@ class OpenFoldDataModule(LightningDataModule):
             with open(distillation_alignment_index_path, "r") as fp:
                 self.distillation_alignment_index = json.load(fp)
 
-    def setup(self):
+    def setup(self, stage=None):
         # Most of the arguments are the same for the three datasets 
         dataset_gen = partial(OpenFoldSingleDataset,
             template_mmcif_dir=self.template_mmcif_dir,
